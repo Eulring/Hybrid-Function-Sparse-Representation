5ad87c1e4ee824fbd851b279832b95ccc40bb840
Eulring <ebeonhard@gmail.com>
2019-01-21 22:04:29 +0800

    1.0

---
+++ config.py
@@ -0,0 +1,7 @@
+import warnings 
+
+class DefaultConfig(object):
+    dataset_route = "/home/e/Eulring/GitProject/Mixture-Function-Sparse-Representation/dataset/Set14"
+    
+    
+opt = DefaultConfig()
